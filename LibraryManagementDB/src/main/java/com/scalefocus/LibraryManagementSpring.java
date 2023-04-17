ebb598bb6f5be1357d87c1ad67a47faaadd71640
Ivaylo37 <ivaylo.vassilev99@gmail.com>
2023-04-18 02:01:27 +0300

    SLM - Added constatnts class and Spring configuration

--- LibraryManagementDB/src/main/java/com/scalefocus/LibraryManagementSpring.java
+++ LibraryManagementDB/src/main/java/com/scalefocus/LibraryManagementSpring.java
@@ -5,27 +5,24 @@ import com.scalefocus.book.BookPresenter;
 import com.scalefocus.client.ClientPresenter;
 import com.scalefocus.order.OrderPresenter;
 import com.scalefocus.util.ConsoleRangeReader;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class LibraryManagement {
+import static com.scalefocus.constants.GlobalConstants.*;
+@SpringBootApplication
+public class LibraryManagementSpring {
 
     private static final BookPresenter bookPresenter = new BookPresenter();
     private static final AuthorPresenter authorPresenter = new AuthorPresenter();
     private static final OrderPresenter orderPresenter = new OrderPresenter();
     private static final ClientPresenter clientPresenter = new ClientPresenter();
-    private static final int MIN_MENU_OPTION = 1;
-    private static final int MAX_MENU_OPTION = 5;
-
-    private static final String GREETING_MESSAGE =
-            "   ****** Choose option: ******* \n" +
-            "   -----------------------------\n" +
-            "   |   1: Book management:     |\n" +
-            "   |   2: Authors management:  |\n" +
-            "   |   3: Orders management:   |\n" +
-            "   |   4: Clients management:  |\n" +
-            "   |   5: Exit                 |\n" +
-            "   -----------------------------";
 
     public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementSpring.class, args);
+        printOptions();
+    }
+
+    private static void printOptions(){
         while (true) {
             System.out.println(GREETING_MESSAGE);
             int choice = ConsoleRangeReader.readInt(MIN_MENU_OPTION, MAX_MENU_OPTION);
---
+++ LibraryManagementDB/src/main/java/com/scalefocus/constants/GlobalConstants.java
@@ -0,0 +1,17 @@
+package com.scalefocus.constants;
+
+public class GlobalConstants {
+
+    public static final int MIN_MENU_OPTION = 1;
+    public static final int MAX_MENU_OPTION = 5;
+
+    public static final String GREETING_MESSAGE =
+            "   ****** Choose option: ******* \n" +
+                    "   -----------------------------\n" +
+                    "   |   1: Book management:     |\n" +
+                    "   |   2: Authors management:  |\n" +
+                    "   |   3: Orders management:   |\n" +
+                    "   |   4: Clients management:  |\n" +
+                    "   |   5: Exit                 |\n" +
+                    "   -----------------------------";
+}
